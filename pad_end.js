9558e29e0b3623e91d082bd09f46cb744b449795
AshikaChadaga <ashikachadaga@gmail.com>
2021-12-06 11:44:14 +0530

    [Ashika]ADDED: Demonstration of padend

---
+++ pad_end.js
@@ -0,0 +1,8 @@
+let price = '1699'.padEnd(7, 0)
+console.log("Price: ",price)
+console.log("Length of price : ", price.length)
+
+
+let price1 = '1699'.padEnd(7)
+console.log("Price: ", price1)
+console.log("Length of price : ", price1.length)
