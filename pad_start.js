aba6593d0a48344be6d5e5cf014b40fd320a2869
AshikaChadaga <ashikachadaga@gmail.com>
2021-12-06 11:41:26 +0530

    [Ashika]ADDED: Demonstration of padstart

---
+++ pad_start.js
@@ -0,0 +1,8 @@
+let price = '1699'.padStart(7, 9)
+console.log("Price: ",price)
+console.log("Length of price : ", price.length)
+
+
+let price1 = '1699'.padStart(7)
+console.log("Price: ", price1)
+console.log("Length of price : ", price1.length)
